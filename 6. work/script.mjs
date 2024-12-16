a2c58a530356df03e005df00111138f89e443064
Faiqshahzad1 <laibanice25@gmail.com>
2024-12-17 00:19:30 +0500

    Feat: watch video 1

---
+++ 6. work/script.mjs
@@ -0,0 +1,20 @@
+let ismarried = true;
+let isactive = false;
+
+
+let userinputtempc = prompt("enter temperature in centigrade");
+
+ userinputtempc = Number(userinputtempc);
+
+ let condition = userinputtempc > 30
+
+ console.log("condition" , condition);
+ 
+ if (condition){
+ 
+    console.log("Garmi ho rahi hai");
+
+}
+else{
+    console.log("garmi nahi horahi hai" );
+}
