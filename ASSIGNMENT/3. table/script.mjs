a9e00073612453e02e5415df35d2668842e32bea
Faiqshahzad1 <laibanice25@gmail.com>
2024-12-25 23:59:17 +0500

    fix: gap issues

--- ASSIGNMENT/3. table/script.mjs
+++ ASSIGNMENT/3. table/script.mjs
@@ -1,14 +1,14 @@
 let table = Number( prompt("write any number "));
-document.write("<br/>",table," x", 1, " =", table*1)
-document.write("<br/>",table," x ", 2, " =", table*2)
-document.write("<br/>",table," x ", 3, " =", table*3)
-document.write("<br/>",table," x ", 4, " =", table*4)
-document.write("<br/>",table," x ", 5, " =", table*5)
-document.write("<br/>",table," x ", 6, " =", table*6)
-document.write("<br/>",table," x ", 7, " =", table*7)
-document.write("<br/>",table," x ", 8, " =", table*8)
-document.write("<br/>",table," x ", 9, " =", table*9)
-document.write("<br/>",table," x ", 10,  "=", table*10)
+document.write("<br/>",table," x ", 1, " = ", table*1)
+document.write("<br/>",table," x ", 2, " = ", table*2)
+document.write("<br/>",table," x ", 3, " = ", table*3)
+document.write("<br/>",table," x ", 4, " = ", table*4)
+document.write("<br/>",table," x ", 5, " = ", table*5)
+document.write("<br/>",table," x ", 6, " = ", table*6)
+document.write("<br/>",table," x ", 7, " = ", table*7)
+document.write("<br/>",table," x ", 8, " = ", table*8)
+document.write("<br/>",table," x ", 9, " = ", table*9)
+document.write("<br/>",table," x ", 10," = ", table*10)
 
 
 
