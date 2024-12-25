3e11fa60f027f06e70dacc3508412c0857aa2531
Faiqshahzad1 <laibanice25@gmail.com>
2024-12-25 19:26:55 +0500

    feat: complete practice of array

---
+++ practice/7. Testing set condition/script.mjs
@@ -0,0 +1,24 @@
+// if (400 > 300 && 5 < 6) {
+//      alert("Come to our tryout!");
+//      }
+//      else {
+//      alert("Come to our cookout!");
+//      }
+//      console.log()
+    
+
+//      if (500 > 300 && 5 < 6 && 16 > 17 && gender === "male") {
+//       alert("Come to our tryout!");
+//       }
+//       else {
+//       alert("Come to our cookout!");
+//       }
+
+      if (2 > 5 || 8 > 2.5 || sport === "football") {
+          alert("Welcome to Bubba State!");
+          }
+          else {
+          alert("Have you looked into appliance repair?");
+          }
+          
+          
---
+++ practice/8. array/script.mjs
@@ -0,0 +1,26 @@
+let pets = ["dog", "cat", "lion", "fish", " gerbil", "Seattle"];
+console.log(pets[3])
+
+console.log(pets.pop());
+
+pets.push("fish", "ferret")
+console.log(pets);
+
+console.log(pets.shift());
+
+pets.unshift("pakistan")
+console.log(pets)
+
+pets.splice(3,0,"monkey")
+console.log(pets)
+
+pets.splice(3,1,"iguana")
+console.log(pets)
+
+let country = ["Pakistan", "india", "indonesia", "america", "china", "japan"]
+let country2 = country.slice(0,5)
+console.log(country2)
+
+
+
+
